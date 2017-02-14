c90e0fdd79404e23de5ce4e84c84e07cc9b35c2c
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-13 22:11:13 -0300

    commit ejercicio casi completo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def paginax
   end
+
+  def submit
+  	# fail params.inspect
+  	User.create({name: params[:name], email: params[:email], age: params[:age]})
+  	redirect_to(pages_paginax_path, notice: "User was saved")
+  end
 end
